121f761cdafdc557876fdce47c8a688f6f13fa48
Agung Dwi Oktavian <118071797+agungdwii@users.noreply.github.com>
2025-06-18 12:23:01 +0700

    tes

--- tests/unit/KelasModelTest.php
+++ tests/unit/KelasModelTest.php
@@ -8,13 +8,11 @@ use App\Models\JurusanModel;
 
 class KelasModelTest extends CIUnitTestCase
 {
-    // Helper untuk memastikan ada Jurusan di database
     private function getJurusanId(): int
     {
         $jurusanModel = new JurusanModel();
         $jurusan = $jurusanModel->first();
         if (!$jurusan) {
-            // PERBAIKAN: Menggunakan nama kolom 'nama_jurusan' yang benar
             return $jurusanModel->insert(['jurusan' => 'Jurusan Default Test ' . time()]);
         }
         return $jurusan['id'];
@@ -41,15 +39,12 @@ class KelasModelTest extends CIUnitTestCase
         $jurusanId = $this->getJurusanId();
         $namaKelas = 'Kelas Find Test ' . time();
         
-        // PERBAIKAN: Menggunakan nama kolom 'nama_kelas' yang benar
         $data = ['kelas' => $namaKelas, 'id_jurusan' => $jurusanId];
         $model->insert($data);
 
-        // PERBAIKAN: Mencari berdasarkan 'nama_kelas'
         $kelas = $model->where('kelas', $namaKelas)->first();
         
         $this->assertIsArray($kelas);
-        // PERBAIKAN: Membandingkan dengan 'nama_kelas'
         $this->assertEquals($namaKelas, $kelas['kelas']);
     }
 
@@ -57,16 +52,13 @@ class KelasModelTest extends CIUnitTestCase
     {
         $model = new KelasModel();
         $jurusanId = $this->getJurusanId();
-        // PERBAIKAN: Menggunakan nama kolom 'nama_kelas' yang benar
         $data = ['kelas' => 'Kelas For Update ' . time(), 'id_jurusan' => $jurusanId];
         $id = $model->insert($data);
 
-        // PERBAIKAN: Menggunakan nama kolom 'nama_kelas' yang benar
         $updatedData = ['kelas' => 'Kelas Updated'];
         $model->update($id, $updatedData);
 
         $updatedKelas = $model->find($id);
-        // PERBAIKAN: Membandingkan dengan 'nama_kelas'
         $this->assertEquals('Kelas Updated', $updatedKelas['kelas']);
     }
     
@@ -74,7 +66,6 @@ class KelasModelTest extends CIUnitTestCase
     {
         $model = new KelasModel();
         $jurusanId = $this->getJurusanId();
-        // PERBAIKAN: Menggunakan nama kolom 'nama_kelas' yang benar
         $data = ['kelas' => 'Kelas For Delete ' . time(), 'id_jurusan' => $jurusanId];
         $id = $model->insert($data);
         
